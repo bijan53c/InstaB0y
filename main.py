91e3f674d9e91768cbb4edb74d98ed894798531a
Bijan Agharafiei <63957104+bijan53c@users.noreply.github.com>
2022-09-02 18:23:04 +0430

    A little update
    Now the bot logs out of account when it's done. So the created session on your account will be terminated.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
 
 
@@ -45,7 +46,7 @@ loginButton = browser.find_element_by_xpath("/html/body/div[1]/section/main/div/
 
 # Read user data (Username and Password to log in to instagram)
 ### Not ready yet
-username = "username"
+username = "Username"
 password = "password"
 
 
@@ -86,9 +87,17 @@ except :
 
 time.sleep(5)
 
+# Process of logging out of account (used when the main bot process is done)
+def logout():
+	time.sleep(5)
+	try:
+		browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/nav/div[2]/div/div/div[3]/div/div[6]/div[1]").click()
+		time.sleep(2)
+		browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/nav/div[2]/div/div/div[3]/div/div[6]/div[2]/div[2]/div[2]/div[6]/div/div/div/div/div").click()
+	except:
+		print(">>> failed to logout")
 
-
-
+#liking new posts
 def likeActionPOST():
 	#Like the post you just opened and go for the next.
 	time.sleep(5)
@@ -107,7 +116,13 @@ def likeActionPOST():
 
 # Post opening function
 def open_first_post() :
-	post_xpath = '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/main/article/div[2]/div/div[1]/div[1]'
+	# first scroll to recent posts
+	print("Scrolling ...")
+	recent_Xpath = browser.find_element_by_xpath('/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/h2')
+	recent_Xpath.location_once_scrolled_into_view
+
+	# Then open the first post 
+	post_xpath = '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div/div[1]/div[1]'
 
 	print ("POSTXPATH =  ",post_xpath)
 	try:
@@ -115,8 +130,17 @@ def open_first_post() :
 	except NoSuchElementException:
 		print(">>> Failed to open post")
 	time.sleep(5)
-	
-	
+
+
+
+# This function closes any open post from the x button on top right of screen
+# Can be used before logout function
+def close_POST():
+	try:
+		browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div").click()
+	except:
+		print(">>> Failed to close post")
+
 
 PostAmount = 10
 #finding hashtags and liking posts
@@ -135,6 +159,10 @@ for hashtag in hashtag_list :
 		print ("Posts liked so far: ",LikedPosts ," __Hashtag: ",hashtag)
 		if LikedPosts == PostAmount :
 			print ("All done")
+			print("Closing current post")
+			close_POST()
+			print ("Logging out...")
+			logout()
 			break
 
 
